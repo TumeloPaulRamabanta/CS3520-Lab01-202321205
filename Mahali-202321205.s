7cd8ac6904c9e1cec321d8162c6ba241b72aae72
TumeloPaulRamabanta <paultpr922@gmail.com>
2025-09-24 03:42:09 +0200

    Update Mahali-202321205.s

--- Mahali-202321205.s
+++ Mahali-202321205.s
@@ -1,34 +1,28 @@
 .data
-	prompt: .asciz "Input number of cattly: "
-	invalid: .asciz "Invalid input try again."
+	prompt: .asciz "Input number of cattle : "
+	invalid: .asciz "Invalid input, try again."
 	lacking: .asciz "Not Enough for Lobola."
 	acceptable: .asciz "Small herd."
-	wealthy: .asciz "Wealthy cattly owner"
+	wealthy: .asciz "Wealthy cattle owner"
 	
 .text
 .globl _start
 _start:
-	# Saved ranges for rating
 	li t1, 10
 	li t2, 20
 	
-	# Print banner
 	la a0, prompt
 	li a7, 4
 	ecall
-	
-	# Get input
-	li a0, 0	# To avoid issues
+
+	li a0, 0
 	li a7, 5
 	ecall
 	
-	# Move Input
 	mv t0, a0
 	
-	# Invalid inout handling
 	bltz t0, Invalid
 	
-	# Poor man
 	blt t0, t1, Lacking
 	blt t0, t2, Acceptable
 	
@@ -51,7 +45,6 @@ Lacking:
 	j Exit
 	
 Invalid:
-	# Print Invalid prompt
 	la a0, invalid
 	li a7, 4
 	ecall
